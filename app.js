6a758fd276c7fd9726b5cd6c5980d2d769eff306
Aleksandra Wilk <oleksandra.wilk@gmail.com>
2020-04-13 16:40:24 +0200

    center watermark

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
-const Jimp = require('jimp'); //pobranie modułu
+const Jimp = require('jimp');
 
 const addTextWatermarkToImage = async function(inputFile, outputFile, text) {
-  const image = await Jimp.read(inputFile); // .read ładowanie plików graficznych . await - kompilacja nie wykona się dopóki plik graficzny nie załaduje się.
-  const font = await Jimp.loadFont(Jimp.FONT_SANS_32_BLACK); //Jimp zezwala na ładowanie własnych fontów tutaj domyślna sans. loadFont - ładuje font i zapisuje do zmiennej
-  image.print(font, 10, 10, text); //dodanie napisu na obrazku
-  await image.quality(100).writeAsync(outputFile); //zapisanie zmienionego obrazka do nowego pliku
+  const image = await Jimp.read(inputFile);
+  const font = await Jimp.loadFont(Jimp.FONT_SANS_32_BLACK);
+  const textData = {
+    text,
+    alignmentX: Jimp.HORIZONTAL_ALIGN_CENTER,
+    alignmentY: Jimp.VERTICAL_ALIGN_MIDDLE,
+  };
+  image.print(font, 0, 0, textData, image.getWidth(), image.getHeight());
+  await image.quality(100).writeAsync(outputFile);
 };
 
 addTextWatermarkToImage('./test.jpg', './test-with-watermark.jpg', 'Hello world')
